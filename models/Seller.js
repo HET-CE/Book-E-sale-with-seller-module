9d0c54c25a5f52407c98e0448c387782b8d17ffd
Het Patel <75133701+HET-CE@users.noreply.github.com>
2022-06-30 21:39:43 +0530

    Add files via upload

---
+++ models/Seller.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const sellerSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  order: [
+    {
+      image: String,
+      title: String,
+      category: String,
+      description: String,
+      price: String,
+      count: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
+    },
+  ],
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Seller", sellerSchema);
